851aeb354830d60fbd94718b35e8daf5e15af8d7
neefra <126564307+neefra@users.noreply.github.com>
2024-10-12 17:16:49 +0000

    Two Files Deleted

--- chapter2/models/Drop_table.sql
+++
@@ -1,3 +0,0 @@
-
-
-drop table if exists 'chapter2'.'total_trips'
--- chapter2/models/total_trips.sql
+++
@@ -1,8 +0,0 @@
-{{
-  config(
-    materialized = 'table'
-    )
-}}
-
-select count(*) total_trip_count from 
-{{ ref('taxi_trips') }}
